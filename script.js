b8e860891ff6a666a3c7f57de8d181fd5651875d
E13 <151720919+E13e@users.noreply.github.com>
2024-12-06 09:22:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const couche0 = document.querySelector(".couche_0");
     const couche1 = document.querySelector(".couche_1");
     const couche2 = document.querySelector(".couche_2");
-    const buttons = document.querySelectorAll("button");
+    const button = document.querySelector("#cv");
 
-    buttons.forEach((button) => {
+    button.forEach((button) => {
         button.addEventListener("click", () => {
-            couche0.classList.add("fullscreen");
-            couche1.classList.add("fullscreen");
-            couche2.classList.add("fullscreen");
+
+            couche0.classList.remove("active");
+            couche1.classList.remove("active");
+            couche2.classList.remove("active");
+
+            if (button.id === "#cv") {
+            couche0.classList.add("active");
+            }
         });
     });
 });
